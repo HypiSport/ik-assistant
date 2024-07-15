0055e15af5ee82bdf9a04adec33d0c5bc96c35c4
HypiSport <pauline.dupont75000@gmail.com>
2024-07-15 09:29:26 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IK Assistant',
+  webDir: 'dist'
+};
+
+export default config;
